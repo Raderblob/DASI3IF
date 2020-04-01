7c83238432ed394cbfbd1d45d60af98845dc73c6
Raderblob <39084971+Raderblob@users.noreply.github.com>
2020-04-01 13:42:11 +0200

    Fix gitIgnore

---
+++ src/main/java/fr/insalyon/dasi/metier/modele/Personne.java
@@ -0,0 +1,91 @@
+package fr.insalyon.dasi.metier.modele;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author DASI Team
+ */
+@MappedSuperclass
+public class Personne implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    protected String nom;
+    protected String prenom;
+    @Column(unique = true)
+    protected String mail;
+    protected String motDePasse;
+    protected String telephoneNumber;
+
+  
+
+    protected Personne() {
+    }
+
+    public Personne(String nom, String prenom, String mail, String motDePasse, String telephoneNumber) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.motDePasse = motDePasse;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+    
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Client : id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", mail=" + mail + ", motDePasse=" + motDePasse + ", tel=" + telephoneNumber ;
+    }
+    
+
+}
