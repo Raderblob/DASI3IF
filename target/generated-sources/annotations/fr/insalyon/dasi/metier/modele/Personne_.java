257310030fdbf91f8d131e5a152d0a361a1a48a3
Raderblob <39084971+Raderblob@users.noreply.github.com>
2020-04-01 14:51:53 +0200

    err Changes

--- target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Personne_.java
+++ target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Personne_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-01T14:43:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-01T14:48:45")
 @StaticMetamodel(Personne.class)
 public class Personne_ { 
 
