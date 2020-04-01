5c3cabbdde46edeb4754de77fb2b8518c6de6463
AureDau <57732337+AureDau@users.noreply.github.com>
2020-04-01 15:06:58 +0200

    ajout test envoi

--- src/main/java/fr/insalyon/dasi/ihm/console/Main.java
+++ src/main/java/fr/insalyon/dasi/ihm/console/Main.java
@@ -6,6 +6,8 @@ import fr.insalyon.dasi.metier.modele.Employee;
 import fr.insalyon.dasi.metier.modele.Gender;
 import fr.insalyon.dasi.metier.modele.Personne;
 import fr.insalyon.dasi.metier.service.Service;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
@@ -13,6 +15,7 @@ import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import util.Message;
 
 /**
  *
@@ -36,7 +39,56 @@ public class Main {
         
         
         JpaUtil.destroy();
+        
+        //envoiMessage();
+        
     }
+    
+    public static void envoiMessage()
+    {
+        
+        //DebugLogger.log("Début des Tests...");
+        
+        StringWriter corps = new StringWriter();
+        PrintWriter mailWriter = new PrintWriter(corps);
+        
+        mailWriter.println("Bonjour,");
+        mailWriter.println();
+        mailWriter.println("  Ceci est un mail destiné à tester l'envoi simulé par affichage sur la console.");
+        mailWriter.println();
+        mailWriter.println("  Cordialement,");
+        mailWriter.println();
+        mailWriter.println("    Yann Gripay");
+
+        Message.envoyerMail(
+                "yann.gripay@insa-lyon.fr",
+                "etudiants.3IF@insa-lyon.fr",
+                "[DASI] Test d'envoi de e-mail",
+                corps.toString()
+            );
+        
+        
+        StringWriter message = new StringWriter();
+        PrintWriter notificationWriter = new PrintWriter(message);
+        
+        notificationWriter.println("Ceci est une notification pour prévenir de 2 choses:");
+        notificationWriter.println("1) NE PAS oublier le poly");
+        notificationWriter.println("2) TESTER au fur et à mesure du développement");
+
+        Message.envoyerNotification(
+                "0988776655",
+                message.toString()
+            );
+        
+        //DebugLogger.log("Fin des Tests...");
+        
+    }
+    
+
+        
+        
+        
+    
 
     public static void afficherPersonne(Personne personne) {
         System.out.println("-> " + personne);
