20e97d0f173a84cab2efe9ddd6e50debf9c7f4db
Raderblob <39084971+Raderblob@users.noreply.github.com>
2020-05-13 18:35:07 +0200

    filters half working (employee Home page)
    Radio buttons work but  search by name not implemented yet

--- DASI-TP-WEB/DASI-TP-WEB/src/main/webapp/JScript/employeeScripts.js
+++ DASI-TP-WEB/DASI-TP-WEB/src/main/webapp/JScript/employeeScripts.js
@@ -95,3 +95,66 @@ function getPastConsultations(){
         alert('Erreur lors du chargement des données: HTTP Code '+error.status);// Popup d'erreur
     });
 }
+
+function applySearchFilters(){
+    const radioEmployee = document.querySelectorAll('#Employee');
+    const radioMedium = document.querySelectorAll('#Medium');
+    const radioClient = document.querySelectorAll('#Client');
+    var ordre="";
+    
+        if(radioEmployee[0].checked){
+            ordre = "getEmployeeList";
+        }else if(radioMedium[0].checked){
+            ordre = "consulter_liste_medium";
+        }else if(radioClient[0].checked){
+            ordre = "getClientList";
+        }
+    
+    $.ajax({// Requête AJAX43                     
+        url:'./Controleur',// URL
+        method:'POST',// Méthode
+        data:{// Paramètres
+        todo:ordre},
+        dataType:'json'// Type de retour attendu
+    })
+    .done(function(response){// Appel OK => "response" contient le résultat JSON
+        const radioEmployee = document.querySelectorAll('#Employee');
+        const radioMedium = document.querySelectorAll('#Medium');
+        const radioClient = document.querySelectorAll('#Client');
+        
+        var title;
+        var htmlIn = "";
+        
+        if(radioEmployee[0].checked){
+            title = "<h2 id='listBoxDivTitle'>Employees</h2>";
+            var employees=response.employees;
+            employees.forEach (function(empl){
+                var b = "<button id='"+ empl.mail +"' onClick='console.log("+ '"' +empl.mail +'"' +")'>" + empl.mail + "</button>";
+                htmlIn = htmlIn + "<br>" + b;
+            });
+        }else if(radioMedium[0].checked){
+            title = "<h2 id='listBoxDivTitle'>Mediums</h2>";
+            var mediums=response.mediums;
+            mediums.forEach (function(med){
+                var b = "<button id='"+ med.nom +"' onClick='console.log("+ '"' +med.nom +'"' +")'>" + med.nom + "</button>";
+                htmlIn = htmlIn + "<br>" + b;
+            });
+        }else if(radioClient[0].checked){
+            title = "<h2 id='listBoxDivTitle'>Clients</h2>";
+            var clients=response.clients;
+            clients.forEach (function(cl){
+                var b = "<button id='"+ cl.mail +"' onClick='console.log("+ '"' +cl.mail +'"' +")'>" + cl.mail + "</button>";
+                htmlIn = htmlIn + "<br>" + b;
+            });
+        }
+        
+
+        
+        $('#listBoxDiv').html(title  + "<br>" + htmlIn);
+
+    })
+    .fail(function(error){// Appel KO => erreur technique à gérer
+        console.log('Erreur:',error);// LOG sur la Console Javascript
+        alert('Erreur lors du chargement des données: HTTP Code '+error.status);// Popup d'erreur
+    });
+}
